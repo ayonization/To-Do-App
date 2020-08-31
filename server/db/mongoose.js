83f566150144ebe7633714c4afbe7c1d057523f6
Ayon Sen <ayon.s2000@gmail.com>
2020-08-31 20:53:08 +0530

    Fixxe bugs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose= require('mongoose');
 
 mongoose.Promise= global.Promise;                
 mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/ToDoApp',{
-    useNewUrlParser,
+    useNewUrlParser:true,
     useUnifiedTopology:true
 });
 
